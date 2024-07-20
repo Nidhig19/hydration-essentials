0a024c5dcd3a17ec922e3d608f78a47d5d58cfe5
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2024-07-20 22:49:39 +0530

    feat: Display Image

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import tensorflow as tf
 import pandas as pd
 from keras.models import Model
-from flask import Flask,app,request,render_template
+from flask import Flask,app,request,redirect,render_template,url_for, send_from_directory
 from keras.preprocessing.image import load_img, img_to_array
 from PIL import Image
 from tensorflow.python.ops.gen_array_ops import Concat
@@ -11,6 +11,9 @@ from keras.models import load_model
 
 model=load_model('models//model.h5')
 app=Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def showdashboard():
@@ -35,7 +38,20 @@ def classification():
         op=['Full water level','Half water level','Overflowing']
         result=str(op[prediction[0]])
         print(result)
-    return render_template('classification.html',prediction=result)
+        return redirect(url_for('predict',file=f.filename,result=result))
+    return render_template('classification.html')
+
+@app.route('/predict.html')
+def predict():
+    file = request.args.get('file')
+    result = request.args.get('result')
+    file_url = url_for('upload', filename=file)
+    print(file_url,result)
+    return render_template('predict.html',file=file_url,prediction=result)
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__=='__main__':
     app.run(debug=True)
